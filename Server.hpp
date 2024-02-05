b9789438a965855e79fe8a2b2240736cc38186ac
root <root@DESKTOP-MOUP08V>
2024-02-05 22:33:14 +0100

    Ajout d'une classe Server initialisant un serveur lors de la création d'un membre

--- Server.hpp
+++ Server.hpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <cstring>
+#include <cstdlib>
+#include <csignal>
+#include <arpa/inet.h>
 #include <sys/socket.h>
-Class Server {
+
+class Server {
     public:
     Server();
     Server(std::string name, std::string password, std::string port);
     Server(const Server& server);
-    Server::operator=(const Server& server);
+    Server const &operator=(const Server& server);
     ~Server();
-    void setName(std::string name);
-    
+    void setServerSocket();
+    void setServerAddr();
+    void bindServer();
+    void listenServer();
+    int const getServerSocket();
     private:
     std::string _name;
     std::string _password;
     std::string _port;
-    int _socket;
+    int _serverSocket;
+    sockaddr_in _serverAddr;
 };
--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <csignal>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+#include "Server.hpp"
 
-const int PORT = 8080;
-const int MAX_CONNECTIONS = 5;
 const int BUFFER_SIZE = 1024;
 int running = 1;
 
@@ -22,40 +14,13 @@ void signalHandler(int signum)
 
 int main()
 {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1)
-    {
-        std::cerr << "Erreur lors de la creation du socket.\n";
-        return (0);
-    }
-
-    sockaddr_in serverAddr;
-    std::memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(8080);
-    // close(serverSocket);
-    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == -1)
-    {
-        std::cerr << "Erreur lors de la liaison du socket \n";
-        close(serverSocket);
-        return (0);
-    }
-
-    if (listen(serverSocket, 5) == -1)
-    {
-        std::cerr << "Erreur lors de la mise en ecoute du socket.\n";
-        close(serverSocket);
-        return (0);
-    }
-
-    std::cout << "Serveur en ecoute sur le port " << PORT << std::endl;
-    // signal(SIGINT, signalHandler);
+    Server server("test", "test", "8080");
+    server.listenServer();
     while (running == 1)
     {
         sockaddr_in clientAddr;
         socklen_t clientAddrLen = sizeof(clientAddr);
-        int clientSocket = accept(serverSocket, reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrLen);
+        int clientSocket = accept(server.getServerSocket(), reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrLen);
         if (clientSocket == -1)
         {
             std::cerr << "Erreur lors de l'acceptation de la connexion.\n";
@@ -73,7 +38,7 @@ int main()
 
         close(clientSocket);
     }
-    close(serverSocket);
+    close(server.getServerSocket());
 
     return (0);
 }
